5575c93574766c1ed05594bb9bb8e5743fea845b
thanhvan1901 <you@example.comnvthanh190199@gmail.com>
2022-09-20 19:56:09 +0700

    ss8

--- ss8/baitap/register_form/src/main/java/codegym/register/repository/IUserRepository.java
+++ ss8/baitap/register_form/src/main/java/codegym/register/repository/IUserRepository.java
@@ -1,2 +1,12 @@
-package codegym.register.repository;public interface IUserRepository {
+package codegym.register.repository;
+
+import codegym.register.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface IUserRepository extends JpaRepository<User , Integer> {
+    Page<User> findAll(Pageable pageable) ;
 }
